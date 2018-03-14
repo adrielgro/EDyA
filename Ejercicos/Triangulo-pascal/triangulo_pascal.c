272440ce856782092af281eb987393dc8e6be9c9
Adriel Guerrero <adrielgro@gmail.com>
2018-03-14 16:45:51 -0700

    Se añadió el triangulo de pascal

--- Ejercicos/Triangulo-pascal/triangulo_pascal.c
+++ Ejercicos/Triangulo-pascal/triangulo_pascal.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 
+int factorial(int n) {
+	if(n < 2)
+		return 1;
+	else
+		return n * factorial(n-1);
+}
+
+int combinacion(int n, int r) {
+	if(r == 1)
+		return n;
+	else {
+		if(n == r)
+			return 1;
+		else
+			return factorial(n) / (factorial(r) * factorial(n - r));
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int n = 8;
+	/*int n = 8;
 	int pascal[n][n];
-	int p = 1;
+	int p = 1;*/
 	
+	int n;
+	printf("Altura del triangulo: ");
+	scanf("%i", &n);
+	printf("\n\n");
+
+	for(int i = 0; i <= n; i++) {
+		for(int j = 1; j < (n-i)+1; j++)
+			printf(" ");
+			
+		for(int k = 0; k <= i; k++)
+			printf	(" %d ", combinacion(i, k));
+			
+		printf("\n\n");
+	}
+	printf("\n\n");
+	
+	
+	
+	/*
 	for (int i = 0; i < n; i++)
 	{
 		int j = 0;
@@ -34,9 +71,9 @@ int main(int argc, char **argv)
 			printf("%d", pascal[i][j]);
 		}
 		printf("\n");
-	}
-	
+	}*/
 	
 	return 0;
 }
+
 
